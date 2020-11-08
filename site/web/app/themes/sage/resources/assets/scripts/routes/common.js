2ccf382de2d86609c822f715ac9bb51614ff3395
Kye Buffery <kyebuffery14@gmail.com>
2020-11-08 12:24:18 +0000

    Added date picker

--- site/web/app/themes/sage/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/sage/resources/assets/scripts/routes/common.js
@@ -1,6 +1,13 @@
+
+import datepickerFactory from 'jquery-datepicker';
+
 export default {
   init() {
+    datepickerFactory($);
     // JavaScript to be fired on all pages
+    $( function() {
+      $( '#datepicker' ).datepicker();
+    } );
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
---
+++ site/web/app/themes/sage/resources/views/partials/booking-form.blade.php
@@ -0,0 +1,3 @@
+<form action="">
+    <label>Date: <input type="text" id="datepicker"></label>
+</form>
