443d0b7c00cc44a2cc49ec3d6b761f5c5286a10a
Kye Buffery <kyebuffery14@gmail.com>
2020-11-08 13:13:17 +0000

    working request

--- site/web/app/themes/sage/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/sage/resources/assets/scripts/routes/common.js
@@ -1,18 +1,24 @@
-
 import datepickerFactory from 'jquery-datepicker';
 
 export default {
-  init() {
-    datepickerFactory($);
-    // JavaScript to be fired on all pages
-    $( function() {
-      $( '#datepicker' ).datepicker({
-        dateFormat: 'dd/mm/yy',
-        minDate: 0,
-      });
-    } );
-  },
-  finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
-  },
+	init() {
+		datepickerFactory($);
+		// JavaScript to be fired on all pages
+		$(function () {
+			$('#datepicker').datepicker({
+				dateFormat: 'yy-m-d',
+				minDate: 0,
+				onSelect: function () {
+					$.get('/index.php/wp-json/v1/booking/times', {
+						date: this.value,
+					}).done(function (data) {
+						console.log('Data Loaded: ' + data);
+					});
+				},
+			});
+		});
+	},
+	finalize() {
+		// JavaScript to be fired on all pages, after page specific JS is fired
+	},
 };
