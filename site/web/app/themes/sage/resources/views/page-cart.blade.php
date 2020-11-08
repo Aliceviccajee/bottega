98b5937f743ea0e496a154ed6f18fd1bde3109f4
Kye Buffery <kyebuffery14@gmail.com>
2020-11-08 10:58:11 +0000

    Cart with items

--- site/web/app/themes/sage/resources/views/page-cart.blade.php
+++ site/web/app/themes/sage/resources/views/page-cart.blade.php
@@ -4,7 +4,10 @@
 @include('partials.page-header')
 @php
 	do_action('get_header', 'shop');
-	do_action('woocommerce_before_main_content');
+	do_action( 'woocommerce_before_cart' );
+	$quantity = WC()->cart->get_cart_contents_count();
+	$sub_total = WC()->cart->get_cart_subtotal();
+
 @endphp
 
 <header class="woocommerce-products-header">
@@ -16,6 +19,33 @@
 	do_action('woocommerce_archive_description');
 	@endphp
 </header>
+<?php do_action( 'woocommerce_before_cart_table' ); ?>
+<div class="shop_table shop_table_responsive cart woocommerce-cart-form__contents">
+	<div class="product-quantity">Quantity: {{$quantity}}</div>
+	<div class="product-subtotal">Total: {!! $sub_total !!}</div>
+	<?php do_action( 'woocommerce_before_cart_contents' ); ?>
+	@foreach (\WC()->cart->get_cart() as $key => $pizza)
+	@php
+		$product = apply_filters( 'woocommerce_cart_item_product', $pizza['data'], $pizza, $key );
+		@endphp
+		{{$product->get_title()}}
+		£{{$product->get_price()}}
+		x {{$pizza['quantity']}}
 
-{{'CART'}}
+	@endforeach
+	<div class="actions col-12 col-md-6">
+		<div class="update-cart d-md-none">
+			<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update', 'woocommerce' ); ?></button>
+		</div>
+
+		<?php do_action( 'woocommerce_cart_actions' ); ?>
+
+		<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+	</div>
+
+@php
+	do_action( 'woocommerce_after_cart_table' );
+	do_action( 'woocommerce_after_cart_contents' );
+	do_action( 'woocommerce_after_cart' );
+@endphp
 @endsection
