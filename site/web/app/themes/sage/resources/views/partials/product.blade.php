8dfa380af6f9428fed7c23526830a907a9545b90
Aliceviccajee <alice@viccajee.com>
2020-11-13 16:59:09 +0000

    menu button reposition

--- site/web/app/themes/sage/resources/views/partials/product.blade.php
+++ site/web/app/themes/sage/resources/views/partials/product.blade.php
@@ -5,6 +5,6 @@
     <div class="product-details">
         <h4 class="product-title">{{$product->get_title()}}</h4>
         <p class="product-discription">{{$product->get_short_description()}}</p>
+				<a href="?add-to-cart={{$product->get_ID()}}" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="{{$product->get_ID()}}" aria-label="Add '{{$product->get_title()}}' to your cart" rel="nofollow">Add to cart</a>
     </div>
-    <a href="?add-to-cart={{$product->get_ID()}}" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="{{$product->get_ID()}}" aria-label="Add '{{$product->get_title()}}' to your cart" rel="nofollow">Add to cart</a>
 </div>
