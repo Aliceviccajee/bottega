bf1479ba34eba8659ff71c66004aa03e50be100f
Kye Buffery <kyebuffery14@gmail.com>
2020-11-13 14:24:21 +0000

    Fixed product arhcive

--- site/web/app/themes/sage/resources/views/woocommerce/archive-product.blade.php
+++ site/web/app/themes/sage/resources/views/woocommerce/archive-product.blade.php
@@ -1,67 +1,42 @@
+@php
+$query = new WC_Product_Query( array(
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'return' => 'objects',
+) );
+$products = $query->get_products();
+@endphp
 
 @extends('layouts.app')
 
 @section('content')
-@include('partials/page-header')
-@include('partials.booking-form')
-
+@include('partials.page-header')
 @php
+	do_action('get_header', 'shop');
 	do_action('woocommerce_before_main_content');
+	the_content();
 @endphp
 
+<header class="woocommerce-products-header">
+	@if(apply_filters('woocommerce_show_page_title', true))
+	<h1 class="woocommerce-products-header__title page-title">{!! woocommerce_page_title(false) !!}</h1>
+	@endif
 
-@php
+	@php
+		do_action('woocommerce_archive_description');
+		@endphp
+	</header>
 
-if ( woocommerce_product_loop() ) {
+	<div class="menu">
+	<div class="products">
+		@foreach ($products as $product)
+			@include('partials.product', ['product' => $product])
+		@endforeach
+	</div>
 
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	// do_action( 'woocommerce_before_shop_loop' );
-
-	woocommerce_product_loop_start();
-
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
-
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action( 'woocommerce_shop_loop' );
-
-			wc_get_template_part( 'content', 'product' );
-		}
-	}
-
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
-
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action( 'woocommerce_after_main_content' );
-
-@endphp
-
+	@php
+		do_action('woocommerce_after_main_content');
+	@endphp
+	@endsection
+</div>
 @endsection
