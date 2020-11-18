4666f6b28d9cb3b81819ea81e1718718b9d3c4f1
Aliceviccajee <alice@viccajee.com>
2020-11-18 16:11:44 +0000

    Social links

--- site/web/app/themes/sage/resources/views/partials/footer.blade.php
+++ site/web/app/themes/sage/resources/views/partials/footer.blade.php
@@ -1,8 +1,11 @@
 <footer class="content-info">
   <div class="container">
+		<div class="logo">
+			<h2>Bottega Pizza</h2>
+		</div>
     <ul class="social">
-      <li><img src="@asset('images/facebook.png')" alt="facebook"></li>
-      <li><img src="@asset('images/instagram.png')" alt="instagram"></li>
+			<a href="https://www.facebook.com/bottega.pizzaBristol/"><li><img src="@asset('images/facebook.png')" alt="facebook"></li></a>
+			<a href="https://www.instagram.com/bottega.pizza/"><li><img src="@asset('images/instagram.png')" alt="instagram"></li></a>
     </ul>
   </div>
 </footer>
