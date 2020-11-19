ee63767cb59123e5e202d6f81707da1257bbdecc
Aliceviccajee <alice@viccajee.com>
2020-11-19 17:01:44 +0000

    Menu logo

--- site/web/app/themes/sage/resources/views/partials/header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/header.blade.php
@@ -1,7 +1,9 @@
 <header class="header">
   <div class="container">
+		<div class="logo">
+			<a href="#">B</a>
+		</div>
     <nav class="nav">
-			<li class="nav-item"><a href="#booking">Menu</a></li>
 			@if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
