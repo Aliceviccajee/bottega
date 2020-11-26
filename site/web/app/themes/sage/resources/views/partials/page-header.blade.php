fd6d813f7c754f43ffbef57c1f0f04173db55f68
Aliceviccajee <alice@viccajee.com>
2020-11-26 15:02:41 +0000

    Copy tweek

--- site/web/app/themes/sage/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/page-header.blade.php
@@ -2,7 +2,7 @@
 	<div class="wrapper-hero">
 		<div class="hero-text">
 			<h1>Bottega Pizza</h1>
-			<h2>Bristols finest artisan pizza</h2>
+			<h2>Bristol's finest artisan pizza</h2>
 			<div><a class="btn" href="#booking">Order now</a></div>
 		</div>
 	</div>
