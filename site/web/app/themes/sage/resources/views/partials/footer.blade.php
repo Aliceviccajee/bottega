bf05ab0f3e6a042867ac8ef83b6c151815ead51f
Aliceviccajee <alice@viccajee.com>
2020-11-26 12:15:59 +0000

    Privacy added to footer

--- site/web/app/themes/sage/resources/views/partials/footer.blade.php
+++ site/web/app/themes/sage/resources/views/partials/footer.blade.php
@@ -1,11 +1,16 @@
 <footer class="content-info">
-  <div class="container">
+  <div class="wrap">
 		<div class="logo">
 			<h2>Bottega Pizza</h2>
 		</div>
+		<h3>Talk to us!</h3>
     <ul class="social">
 			<a href="https://www.facebook.com/bottega.pizzaBristol/"><li><img src="@asset('images/facebook.png')" alt="facebook"></li></a>
 			<a href="https://www.instagram.com/bottega.pizza/"><li><img src="@asset('images/instagram.png')" alt="instagram"></li></a>
-    </ul>
+		</ul>
+		<ul class="contact">
+			<li><a href="mailto:hello@bottegapizza.co.uk">hello@bottegapizza.co.uk</a></li>
+			<li><a href="https://bottegapizza.co.uk/privacy-policy-2/">Privacy Policy</li>
+		</ul>
   </div>
 </footer>
