39c43426a58e4c398eab3076d343119d61dd58a5
Aliceviccajee <alice@viccajee.com>
2020-12-02 08:15:37 +0000

    Time slot amend

--- site/web/app/mu-plugins/rest-client/app/Controllers/Booking.php
+++ site/web/app/mu-plugins/rest-client/app/Controllers/Booking.php
@@ -8,15 +8,14 @@ use RestClient\App\Models\Booking;
 
 class BookingController {
 	private static $booking_slots = [
-		"17:00",
-		"17:30",
-		"18:00",
-		"18:30",
-		"19:00",
-		"19:30",
-		"20:00",
-		"20:30",
-		"21:00",
+		"17:00-17:30",
+		"17:30-18:00",
+		"18:00-18:30",
+		"18:30-19:00",
+		"19:00-19:30",
+		"19:30-20:00",
+		"20:00-20:30",
+		"20:30-21:00",
 	];
 	/**
 	 * Base Controller constructor.
