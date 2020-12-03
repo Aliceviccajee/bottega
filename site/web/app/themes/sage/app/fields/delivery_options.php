de0ca0973c0be8df216fc11225b2e47fe0b8375d
Kye Buffery <kyebuffery14@gmail.com>
2020-12-03 15:50:14 +0000

    Added delivery radius

---
+++ site/web/app/themes/sage/app/fields/delivery_options.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+acf_add_options_page([
+	'page_title' => 'Delivery Settings',
+	'menu_title' => 'Delivery Settings',
+	'menu_slug'  => 'delivery_settings',
+	'capability' => 'edit_theme_options',
+	'position'   => '26',
+	'autoload'   => true
+]);
+
+$fields = new FieldsBuilder('delivery_settings');
+
+$fields
+	->setLocation('options_page', '==', 'delivery_settings')
+	->addNumber('delivery_radius', [
+		'label' => 'Delivery Radius in miles',
+		'required' => 1,
+		'default_value' => '3.8',
+		'step' => '0.1',
+]);
+
+return $fields;
